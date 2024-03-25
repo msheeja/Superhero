ba72d5dabd2c2f50512b890c0be69a912acd5e33
Sheeja Mani <Sheeja.Mani@in.frauscher.com>
2024-03-25 14:14:58 +0530

    Update to use secure connection

--- apiModule.js
+++ apiModule.js
@@ -2,7 +2,7 @@ const timeStamp = 1711279729766;
 const hash = "aa32fee78c24c1bb7ec0f0c3b5d1c0ef";
 const apikey = "a2cc0d45f9962ec65a70f671338ebc74";
 const apiParams = `ts=${timeStamp}&apikey=${apikey}&hash=${hash}`;
-const charactersUrl = "http://gateway.marvel.com/v1/public/characters";
+const charactersUrl = "https://gateway.marvel.com/v1/public/characters";
 
 async function getAllCharactersFromApi() {
   const url = `${charactersUrl}?${apiParams}`;
